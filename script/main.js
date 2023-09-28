2431c2ace38edad8a549888b4d58f78cbcf912b2
Elian <eliangonzalo98@gmail.com>
2023-09-28 11:31:05 -0300

    V 0.5

---
+++ script/main.js
@@ -0,0 +1,9 @@
+// window.addEventListener("scroll", function () {
+//   let frame1 = document.getElementById("scroll-behavior");
+//   let posFrame1 = frame1.getBoundingClientRect().top;
+
+//   if (posFrame1 < 0) {
+//     frame1.style.cssText = 'position: fixed; top: 0; left: 0; right: 0; bottom: 0;'
+
+//   }
+// });
